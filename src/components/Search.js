64494ae21f18fe2be9c739578633dee9ddd2962f
HitomiTahara <hitomi.20220218@gmail.com>
2022-10-12 23:13:52 +0900

    Render Wikipedia API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,37 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programing");
+  const [results, setResults] = useState([]);
 
-  console.log("I run every");
   useEffect(() => {
-    console.log("I run after every initial");
-  }, []);
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   const onInputChange = (e) => {
     setTerm(e.target.value);
@@ -28,6 +53,9 @@ const Search = () => {
             onChange={onInputChange}
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   );
