448523c44e01f1c4708353436fa6bad45a6f69e1
HitomiTahara <hitomi.20220218@gmail.com>
2022-10-13 09:47:54 +0900

    Show search results

--- src/components/Search.js
+++ src/components/Search.js
@@ -18,16 +18,25 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
-
-    search();
+    if (term) {
+      search();
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
@@ -54,9 +63,7 @@ const Search = () => {
           />
         </div>
       </div>
-      <div className="ui celled list">
-        {renderedResults}
-      </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
