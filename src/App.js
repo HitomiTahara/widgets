5ba5d022c0e8c9926d88db91c634e06feecaa4e6
HitomiTahara <hitomi.20220218@gmail.com>
2022-10-23 10:47:17 +0900

    Add to open new tab

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
-import Translate from "./components/translate";
+import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
+
 
 const items = [
   {
@@ -41,31 +43,11 @@ const showAccordion = () => {
   }
 };
 
-const showList = () => {
-  if (window.location.pathname === "/list") {
-    return <Search />;
-  }
-};
-const showDropdown = () => {
-  if (window.location.pathname === "/dropdown") {
-    return <Dropdown />;
-  }
-};
-
-const showTranslate = () => {
-  if (window.location.pathname === "/translate") {
-    return <Translate />;
-  }
-};
-
-const showComponent = (route, component) => {
-  return window.location.pathname === route ? component : null;
-};
-
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
